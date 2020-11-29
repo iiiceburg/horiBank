02bdb29c67a8a7036f8e024f2e3018010c908f5b
Eakachai Singhacharee <ounhanan2015@gmail.com>
2020-11-29 17:27:18 +0700

    day3-transfer

--- src/index.py
+++ src/index.py
@@ -1,14 +1,21 @@
 from controllers.login import Login
 from controllers.register import Register
-def welcome():
-    lines = "="*20
-    print(lines + " Welcome to Hori+ Bank " + lines)
-    print("\n1.Login      2.Register")
+
+lines = "="*20
+head = lines + " Welcome to Hori+ Bank " + lines
+sep = "\n"+"="*len(head)  # seperate lines
+
+def welcome():         
+    print(head)
+    print("\n"+" "*len(lines)+"1.Login      2.Register")
+    print(sep)
+    
 
 def menu():
-    menu = input("Please select 1 or 2 : ")
+    menu = input("\n Please select 1 or 2 : ")
     while menu not in ["1","2"] :
         menu = input("Please select 1 or 2 : ")   
+        print(sep)
     if menu == "1" :
         print("\n=== Login === \n ")
         Login.login()
--- src/services/services.py
+++ src/services/services.py
@@ -1,15 +1,20 @@
 class services():
     def servMenu():
+        from datetime import datetime
+        now = datetime.now()
+        timestamp = datetime.timestamp(now)
+        dt_object = datetime.fromtimestamp(timestamp)
         lst = []
-        pin,balance,accNumber = [],[],[]
+        name,pin,balance,accNumber = [],[],[],[]
         with open("./db/account.txt","r") as file :
             data = file.read().splitlines()            
             for line in data :
                 lst = line.split()
+                fullname = lst[0] + " " + lst[1]
+                name.append(fullname)
                 balance.append(lst[8])
                 pin.append(lst[9])
                 accNumber.append(lst[2])
-
             print(balance,pin)
         lines = "="*20
         head = lines + " PLEASE CHOOSE SERVICES " + lines
@@ -26,12 +31,13 @@ class services():
             print("Invalid! Please try again!")
             menu = input("Please choose services 1,2,3,4 or 5 :")
         if menu == "1" :
-            print(balance)
+            print("\nTransfer service")
             account = input("Enter Account number to transfer : ")
             while account not in accNumber:
                 print("Not found account number for transfer!")
-                account = input("Enter Account number to transfer : ") 
+                account = input("Enter Account number to transfer : ")
             endPoint = accNumber.index(account)
+            print("Account Name : %s"%name[endPoint])
             blcData = balance[endPoint] # Find position of balance by endpoint
             print("Hori+ can transfer money 0.01 to 10,000,000.00")
             amount = float(input("Enter amount : "))
@@ -41,10 +47,22 @@ class services():
             total = float(blcData) + amount
             # print("Total = %.2f"%total)
             balance[endPoint] = str(total)
+            lst2 = [] #list for check and transfer
+            for line in data :
+                lst2 = line.split()
+                if account in lst2:
+                    with open("./db/log_transactionTransfer.txt","a+") as file:
+                        status =  str(dt_object) +" Status : Transfer completed "  + " Name : " + name[endPoint] + " Account : " + account + " Amount : " + str(amount) + " Balance : " +  str(total)
+                        file.writelines(status + "\n")
+                print(lst2)
             print(balance)
             
         elif menu == "2":
-            pass
+            print("\nWithdrawal Cash service")
+            pinAuth = input("Enter pin : ")
+            while pinAuth not in pin:
+                print("Invalid pin! Please try again.")
+                pinAuth = input("Enter pin : ")
 
             
 
